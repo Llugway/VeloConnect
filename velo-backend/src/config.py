7c44f203a68f4db1d86f42ae2a3af9ed1e8ed511
Lugway <lerouge.antonin@gmail.com>
2026-01-22 21:34:37 +0100

    backend deploy fix

--- velo-backend/src/config.py
+++ velo-backend/src/config.py
@@ -4,6 +4,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/velo-prod.db'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'veloconnect123'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False          # True for SQL Debug
