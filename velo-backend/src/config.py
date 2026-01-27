27002a1145e8a890873684b27f89909138d1997f
Lugway <lerouge.antonin@gmail.com>
2026-01-27 07:10:49 +0100

    clean repo

--- velo-backend/src/config.py
+++ velo-backend/src/config.py
@@ -1,16 +1,15 @@
 from datetime import timedelta
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance/velo-prod.db')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'veloconnect123'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False          # True for SQL Debug
 
     # JWT
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwt-cle-tres-securisee-change-moi'
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwtsecret123'
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
 
     @staticmethod
@@ -20,21 +19,16 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'velo-dev.db')
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'instance/velo-dev.db')
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # DB in RAM for unit tests
 
-
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'instance', 'velo-prod.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'instance/velo-prod.db')
 
-
-# Mapping des environnements
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
